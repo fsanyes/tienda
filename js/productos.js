d0adc380bbbd5b1aee43ed77fd91e641ee19457f
fsanyes <fsanyes@g.educaand.es>
2023-02-22 23:39:22 +0100

    implementando carrito

--- js/productos.js
+++ js/productos.js
@@ -1,13 +1,50 @@
+const objetos = document.getElementById('objetos');
+const templateCard = document.getElementById('template-card').content
+const fragmento = document.createDocumentFragment();
+
+let carrito = {}
+
+const setCarrito = objeto => {
+    
+}
+
+const sumaCarrito = e => {
+    console.log(e.target);
+    // console.log(e.target.classList.contains('comprar'));
+    if (e.target.classList.contains('comprar')) {
+        // TODO: seguir con la implementación del carrito
+    }
+    e.stopPropagation();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
 })
 
+objetos.addEventListener('click', objeto => {
+    sumaCarrito(objeto);
+})
 const fetchData = async () => {
     try {
         const resul = await fetch('/src/objetos.json')
         const datos = await resul.json();
-        console.log(datos);
+        // console.log(datos);
+        pintarObjetos(datos);
     } catch (error) {
         console.log("error al cargar archivo JSON")
     }
 }
+
+const pintarObjetos = datos => {
+    console.log(datos);
+    datos.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.nombre;
+        templateCard.querySelector('#precio').textContent = producto.precio;
+        templateCard.querySelector('img').setAttribute("src", producto.imagenes[0])
+        templateCard.querySelector('button').dataset.id = producto.id;
+        const clon = templateCard.cloneNode(true);
+        fragmento.appendChild(clon);
+    })
+    objetos.appendChild(fragmento);
+}
+
